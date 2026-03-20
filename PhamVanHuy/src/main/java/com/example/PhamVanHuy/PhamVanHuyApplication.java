ee4c028283bc1629dd537ad85076199e229a9a72
Huycode11 <phamvanhuyhuy2020@gmail.com>
2026-03-20 15:46:08 +0700

    baitap

---
+++ PhamVanHuy/src/main/java/com/example/PhamVanHuy/PhamVanHuyApplication.java
@@ -0,0 +1,13 @@
+package com.example.PhamVanHuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamVanHuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamVanHuyApplication.class, args);
+	}
+
+}
---
+++ PhamVanHuy/src/test/java/com/example/PhamVanHuy/PhamVanHuyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PhamVanHuy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamVanHuyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
